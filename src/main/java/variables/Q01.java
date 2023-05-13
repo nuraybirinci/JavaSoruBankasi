64677654c311e7f8a1fddfafa195c8adfedeb85d
Nuray Birinci <nuraybirinci6761@gmail.com>
2023-05-13 15:34:24 +0300

    13.05.23

---
+++ src/main/java/variables/Q01.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Q01 {
+    public static void main(String[] args) {
+        /*
+        Herhangi bir 3 öğenin fiyatları için double data tipinde değişkenler oluşturunuz.
+        Fiyatlarıntoplamını konsola yazdırınız.
+        */
+        double a = 4.97432;
+        double b = 0.675;
+        double c = 8.927332;
+        System.out.println("fiyatların toplamı "+(a+b+c));
+    }
+}
